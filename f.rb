e2d4a0f28f2b19eec407845b2e91772adf3badf5
tk <kexf7pqsdu@snkmail.com>
2013-06-07 22:53:16 -0500

    Use logger for Faraday

--- f.rb
+++ f.rb
@@ -9,6 +9,7 @@ def c
   conn = Faraday.new(url: 'https://www.toggl.com/api/v8')
   conn.headers = {"Content-Type" => "application/json"}
   conn.basic_auth `cat ~/.toggl`, 'api_token'
+  conn.use Faraday::Response::Logger, Logger.new('faraday_f.log')
   conn
 end
 
