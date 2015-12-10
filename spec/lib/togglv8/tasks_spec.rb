a5341bacd8fcdbee816f81a5b1b614b9a1c2d930
tk <kexf7pqsdu@snkmail.com>
2015-12-10 03:27:47 -0600

    Update tasks_spec.

--- spec/lib/togglv8/tasks_spec.rb
+++ spec/lib/togglv8/tasks_spec.rb
@@ -3,7 +3,7 @@ describe 'Tasks', :pro_account do
     @toggl = TogglV8::API.new(Testing::API_TOKEN)
     @workspaces = @toggl.workspaces
     @workspace_id = @workspaces.first['id']
-    @project = @toggl.create_project({ name: 'project with a task', wid: @workspace_id })
+    @project = @toggl.create_project({ 'name' => 'project with a task', 'wid' => @workspace_id })
   end
 
   after :all do
@@ -12,13 +12,13 @@ describe 'Tasks', :pro_account do
 
   context 'new task' do
     before :all do
-      @task = @toggl.create_task({ name: 'new task', pid: @project['id'] })
-      task_ids = @toggl.get_project_tasks(@project['id']).map { |t| t['id'] }
-      expect(task_ids).to eq [ @task['id'] ]
+      @task = @toggl.create_task({ 'name' => 'new task', 'pid' => @project['id'] })
+      @task_ids = @toggl.get_project_tasks(@project['id']).map { |t| t['id'] }
+      expect(@task_ids).to eq [ @task['id'] ]
     end
 
     after :all do
-      TogglV8SpecHelper.delete_all_tasks(@toggl)
+      @toggl.delete_tasks(@task_ids)
       tasks = @toggl.get_project_tasks(@project['id'])
       expect(tasks).to be_empty
     end
@@ -33,13 +33,15 @@ describe 'Tasks', :pro_account do
 
     it 'gets a task' do
       task = @toggl.get_task(@task['id'])
-      expect(task).to eq @task
+      expect(task).to include('at') # 'at' is last updated timestamp
+      task.delete('at')             # 'at' is not included in POST response
+      expect(task).to eq @task      # compare POST and GET responses
     end
   end
 
   context 'updated task' do
     before :each do
-      @task = @toggl.create_task({ name: 'task to update', pid: @project['id'] })
+      @task = @toggl.create_task({ 'name' => 'task to update', 'pid' => @project['id'] })
     end
 
     after :each do
@@ -58,29 +60,30 @@ describe 'Tasks', :pro_account do
 
   context 'multiple tasks' do
     before :each do
-      @task1 = @toggl.create_task({ name: 'task1', pid: @project['id'] })
-      @task2 = @toggl.create_task({ name: 'task2', pid: @project['id'] })
-      @task3 = @toggl.create_task({ name: 'task3', pid: @project['id'] })
+      timestamp = Time.now.strftime("%H%M%S.%9N")
+      @task1 = @toggl.create_task({ 'name' => "task1-#{timestamp}", 'pid' => @project['id'] })
+      @task2 = @toggl.create_task({ 'name' => "task2-#{timestamp}", 'pid' => @project['id'] })
+      @task3 = @toggl.create_task({ 'name' => "task3-#{timestamp}", 'pid' => @project['id'] })
+      @task_ids = [ @task1['id'], @task2['id'], @task3['id'] ]
     end
 
     after :all do
-      TogglV8SpecHelper.delete_all_tasks(@toggl)
+      @toggl.delete_tasks(@task_ids)
     end
 
     it 'updates multiple tasks' do
       # start with 3 active tasks
       tasks = @toggl.get_project_tasks(@project['id'])
       active_flags = tasks.map { |t| t['active'] }
-      expect().to match_array(['true', 'true', 'true'])
+      expect(active_flags).to match_array([true, true, true])
 
       t_ids = [@task1, @task2, @task3].map { |t| t['id'] }
-      params = { 'active': true }
+      params = { 'active': false }
       @toggl.update_tasks(t_ids, params)
 
-      # end with 3 inactive tasks
+      # end with no active tasks
       tasks = @toggl.get_project_tasks(@project['id'])
-      active_flags = tasks.map { |t| t['active'] }
-      expect().to match_array(['true', 'true', 'true'])
+      expect(tasks).to be_empty
     end
 
     it 'deletes multiple tasks' do
@@ -90,8 +93,8 @@ describe 'Tasks', :pro_account do
       t_ids = [@task1, @task2, @task3].map { |t| t['id'] }
       @toggl.delete_tasks(t_ids)
 
-      # end with no tasks
-      expect(@toggl.get_project_tasks(@project['id']).length).to be_empty
+      # end with no active tasks
+      expect(@toggl.get_project_tasks(@project['id'])).to be_empty
     end
   end
 end
