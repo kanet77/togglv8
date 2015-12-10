d29fc08021d8917ed85d2893170e6fd7cb8e8add
tk <kexf7pqsdu@snkmail.com>
2015-12-10 06:02:38 -0600

    Add spec for my_deleted_projects.

--- spec/lib/togglv8/users_spec.rb
+++ spec/lib/togglv8/users_spec.rb
@@ -2,6 +2,8 @@ describe 'Users' do
   before :all do
     @toggl = TogglV8::API.new(Testing::API_TOKEN)
     @user = @toggl.me(all=true)
+    @workspaces = @toggl.workspaces
+    @workspace_id = @workspaces.first['id']
   end
 
   it 'returns /me' do
@@ -14,32 +16,52 @@ describe 'Users' do
     # expect(@user['workspaces'].first['name']).to eq "togglv8's workspace"
   end
 
-  it 'returns /my_clients' do
+  it 'returns my_clients' do
     my_clients = @toggl.my_clients(@user)
     expect(my_clients).to be_empty
   end
 
-  it 'returns /my_projects' do
+  it 'returns my_projects' do
     my_projects = @toggl.my_projects(@user)
     expect(my_projects).to be_empty
   end
 
-  it 'returns /my_tags' do
+  it 'returns my_projects and my_deleted_projects' do
+    # Create project
+    project = @toggl.create_project({ 'name' => 'my project', 'wid' => @workspace_id })
+
+    my_project_ids         = @toggl.my_projects.map { |p| p['id'] }
+    my_deleted_project_ids = @toggl.my_deleted_projects.map { |p| p['id'] }
+
+    expect(my_project_ids).to eq [ project['id'] ]
+    expect(my_deleted_project_ids).not_to include(project['id'])
+
+    # Delete project
+    @toggl.delete_project(project['id'])
+
+    my_project_ids         = @toggl.my_projects.map { |p| p['id'] }
+    my_deleted_project_ids = @toggl.my_deleted_projects.map { |p| p['id'] }
+
+    expect(my_project_ids).to eq []
+    expect(my_deleted_project_ids).to include(project['id'])
+  end
+
+  it 'returns my_tags' do
     my_tags = @toggl.my_tags(@user)
     expect(my_tags).to be_empty
   end
 
-  it 'returns /my_tasks' do
+  it 'returns my_tasks' do
     my_tasks = @toggl.my_tasks(@user)
     expect(my_tasks).to be_empty
   end
 
-  it 'returns /my_time_entries' do
+  it 'returns my_time_entries' do
     my_time_entries = @toggl.my_time_entries(@user)
     expect(my_time_entries).to be_empty
   end
 
-  it 'returns /my_workspaces' do
+  it 'returns my_workspaces' do
     my_workspaces = @toggl.my_workspaces(@user)
     expect(my_workspaces.length).to eq 1
   end
